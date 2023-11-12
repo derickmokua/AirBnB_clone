5229046adfc8ffef531506500a4f325d8e96c2d2
derickmokua <126662232+derickmokua@users.noreply.github.com>
2023-11-12 15:12:41 +0300

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Defines unittests for models/review.py.
+"""
+Defines unit tests for models/review.py.
 
 Unittest classes:
     TestReview_instantiation
     TestReview_save
     TestReview_to_dict
 """
+
 import os
 import models
 import unittest
@@ -15,7 +17,7 @@ from models.review import Review
 
 
 class TestReview_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the Review class."""
+    """Unit tests to verify the instantiation of the Review class."""
 
     def test_no_args_instantiates(self):
         self.assertEqual(Review, type(Review()))
@@ -97,7 +99,7 @@ class TestReview_instantiation(unittest.TestCase):
 
 
 class TestReview_save(unittest.TestCase):
-    """Unittests for testing save method of the Review class."""
+    """Unit tests to evaluate the save method of the Review class."""
 
     @classmethod
     def setUp(self):
@@ -148,7 +150,7 @@ class TestReview_save(unittest.TestCase):
 
 
 class TestReview_to_dict(unittest.TestCase):
-    """Unittests for testing to_dict method of the Review class."""
+    """Unit tests to assess the to_dict method of the Review class."""
 
     def test_to_dict_type(self):
         self.assertTrue(dict, type(Review().to_dict()))
