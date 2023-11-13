1c168d0a902fbe5b1422d28d2e415deb667be6d1
derickmokua <126662232+derickmokua@users.noreply.github.com>
2023-11-13 05:05:32 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
+"""Defines Amenity class."""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
+    """Represent an amenity
 
     Attributes:
-        name (str): The name of the amenity.
+        name (str): The name of the amenity
     """
 
     name = ""
